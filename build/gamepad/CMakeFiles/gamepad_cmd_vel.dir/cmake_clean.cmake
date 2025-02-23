ec3f6f243824d3325a10df561abaf7df4ef12b64
changfengpro <2728019272@qq.com>
2025-02-23 19:20:01 +0800

    哨兵手柄控制节点

---
+++ build/gamepad/CMakeFiles/gamepad_cmd_vel.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gamepad_cmd_vel.dir/src/gamepad.cpp.o"
+  "CMakeFiles/gamepad_cmd_vel.dir/src/gamepad.cpp.o.d"
+  "CMakeFiles/gamepad_cmd_vel.dir/src/gamepad_cmd_vel.cpp.o"
+  "CMakeFiles/gamepad_cmd_vel.dir/src/gamepad_cmd_vel.cpp.o.d"
+  "gamepad_cmd_vel"
+  "gamepad_cmd_vel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gamepad_cmd_vel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/gamepad/CMakeFiles/gamepad_test_node.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gamepad_test_node.dir/src/gamepad.cpp.o"
+  "CMakeFiles/gamepad_test_node.dir/src/gamepad.cpp.o.d"
+  "CMakeFiles/gamepad_test_node.dir/src/gamepad_test_node.cpp.o"
+  "CMakeFiles/gamepad_test_node.dir/src/gamepad_test_node.cpp.o.d"
+  "gamepad_test_node"
+  "gamepad_test_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gamepad_test_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
